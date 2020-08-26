dd7b226bd4970f47942a20d5c76eaee2433b2344
SamirAllahverdi <a_nazim2007@mail.ru>
2020-08-26 18:56:23 +0400

    --- versioning added.

---
+++ src/main/java/com/example/model/versioning/Name.java
@@ -0,0 +1,11 @@
+package com.example.model.versioning;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Name {
+	private String firstName;
+	private String lastName;
+}
---
+++ src/main/java/com/example/model/versioning/PersonV1.java
@@ -0,0 +1,10 @@
+package com.example.model.versioning;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class PersonV1 {
+    private String name;
+}
