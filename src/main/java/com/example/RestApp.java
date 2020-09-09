29106dba4888c2d340280dd8e83dd7914d030bba
SamirAllahverdi <a_nazim2007@mail.ru>
2020-09-09 14:46:14 +0400

    ---- READ.MD added

--- src/main/java/com/example/RestApp.java
+++ src/main/java/com/example/RestApp.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestApp {
 
     public static void main(String[] args) {
+
         SpringApplication.run(RestApp.class, args);
+
     }
-
-
 }
